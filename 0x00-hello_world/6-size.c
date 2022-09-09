a4f1861f7e47e63284ddcc20cd998de24a87334a
ziyaulhaqq <abdulhaqqahmad@gmail.com>
2022-09-08 18:04:54 -0700

    c language

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,11 @@
  */
 main(void)
 {
+	int a;
+	char b;
+	float c;
+	double d;
+
 	printf("size of int: %zu bytes\n", sizeof(intType));
 	printf("size of char: %zu bytes\n", sizeof(charType));
 	printf("size of float: %zu bytes\n", sizeof(floatType));
